d68859e87e45edfbf67102d5073ad00d3fdaa101
Soukaigiwar <54086765+Soukaigiwar@users.noreply.github.com>
2021-08-27 18:52:15 -0300

    ft_memcmp.c pt-br

--- comentado_pt-br/ft_memcmp.c
+++ comentado_pt-br/ft_memcmp.c
@@ -10,9 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// A função memcmp() compara os primeiros bytes n (representados como unsigned
-// char) das posições da memória fornecidas pelos ponteiros *str1 e *str2.
-// A comparaç
+// A função memcmp() compara os primeiros bytes n das posições da memória 
+// fornecidas pelos ponteiros *str1 e *str2.
+// A comparação se dá pela diferença na tabela ascii dos caracteres passados. 
+// A função retorna 0 se não houver diferenças ou qualquer número diferente,
+// caso os caracteres sejam diferentes.
 
 #include "libft.h"
 
@@ -20,10 +22,17 @@ int	ft_memcmp(const void *str1, const void *str2, size_t n)
 {
 	if (!n)
 		return (0);
+
+	// compara se ponteiro de str1 igual ponteiro de str2
+	// e se número de posições nos ponteiros chegou ao fim.
 	while (*(unsigned char *)str1 == *(unsigned char *)str2 && --n)
 	{
 		str1++;
 		str2++;
 	}
+	// retorna a diferença entre eles na tabela ascii.
+	// ex: 
+	// ('a' = 97 (ascii)) - ('a' = 97 (ascii)) = 0 --> caracteres iguais
+	// ('b' = 98 (ascii)) - ('a' = 97 (ascii)) = 1 --> caracteres diferentes
 	return (*(unsigned char *)str1 - *(unsigned char *)str2);
 }
