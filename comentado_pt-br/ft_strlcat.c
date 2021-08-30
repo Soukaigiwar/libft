183a3264c296d6741560e0ebf72e1e81b722c6dc
Soukaigiwar <54086765+Soukaigiwar@users.noreply.github.com>
2021-08-30 08:28:59 -0300

    ft_strlcat.c

--- comentado_pt-br/ft_strlcat.c
+++ comentado_pt-br/ft_strlcat.c
@@ -11,16 +11,6 @@
 /* ************************************************************************** */
 
 
-The strlcpy() and strlcat() functions copy and concatenate strings respectively. 
-They are designed to be safer, more consistent, and less error prone 
-replacements for strncpy(3) and strncat(3). Unlike those functions, 
-strlcpy() and strlcat() take the full size of the buffer (not just the length) 
-and guarantee to NUL-terminate the result (as long as size is larger than 0 or,
-in the case of strlcat(), as long as there is at least one byte free in dst). 
-Note that a byte for the NUL should be included in size. Also note that strlcpy() 
-and strlcat() only operate on true ''C'' strings. This means that for strlcpy() 
-src must be NUL-terminated and for strlcat() both src and dst must be NUL-terminated.
-
 The strlcpy() function copies up to size - 1 characters from the NUL-terminated 
 string src to dst, NUL-terminating the result.
 
@@ -39,6 +29,8 @@ not be NUL-terminated (since there was no space for the NUL). This keeps strlcat
 from running off the end of a string. In practice this should not happen (as it 
 means that either size is incorrect or that dst is not a proper ''C'' string). 
 The check exists to prevent potential security problems in incorrect code.
+
+
 // A função ft_strlcat concatena a string *src ao final de *dst.
 // Ela previne que o comprimento da string final seja o suficiente pra caber *src
 #include "libft.h"
@@ -53,16 +45,18 @@ size_t	ft_strlcat(char	*dst, const char	*src, size_t	size)
 	dst_size = 0;
 
 	// enquanto percorre dst e dst_len é menor que o size passado como parâmetro
-	// incrementa dst_len
+	// incrementa dst_len para ao final ter o tamanho compatível com a soma
+	// das duas strings juntas.
 	while (dst[dst_size] && dst_size < size)
 		++dst_size;
 	// se size passado como parâmetro for menor que a dst_size + src_size + 1, 
-	// entra no while alterar nada, pois desse jeito não caberia o caracter '\0' no fim de *dst
+	// entra no while e copia um por um os caracteres.
 	while ((src[src_size]) && ((dst_size + src_size + 1) < size))
 	{
 		dst[dst_size + src_size] = src[src_size];
 		src_size++;
 	}
+	// ao final adiciona o caracter nulo na última posição de dst
 	if (dst_size != size)
 		dst[dst_size + src_size] = '\0';
 	return (dst_size + ft_strlen(src));
